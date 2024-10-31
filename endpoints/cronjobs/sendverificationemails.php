7f47a8284866afb816b30583ec7d13e086ed984d
ChrisVo0505 <chris.vo0505@gmail.com>
2024-10-31 23:42:17 +1100

    edit mail verification

--- endpoints/cronjobs/sendverificationemails.php
+++ endpoints/cronjobs/sendverificationemails.php
@@ -56,10 +56,10 @@ if ($rows) {
             foreach ($rows as $user) {
                 $mail->addAddress($user['email']);
                 $mail->isHTML(true);
-                $mail->Subject = 'Wallos - Email Verification';
-                $mail->Body = '<img src="' . $server_url . '/images/siteicons/wallos.png" alt="Logo" />
+                $mail->Subject = 'Submate - Email Verification';
+                $mail->Body = '<img src="' . $server_url . '/images/icon/logo.png" alt="Logo" />
                     <br>
-                    Registration on Wallos was successful.
+                    Registration on Submate was successful.
                     <br>
                     Please click the following link to verify your email: <a href="' . $server_url . '/verifyemail.php?email=' . $user['email'] . '&token=' . $user['token'] . '">Verify Email</a>';
 
